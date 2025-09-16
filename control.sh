699e5f988f02e5e4c1604b1669ba8018ab3c6b5a
hildanku <hildankutomo@gmail.com>
2025-09-16 21:47:34 +0700

    feat: add feature to control fan & status fan level

---
+++ control.sh
@@ -0,0 +1,23 @@
+#1/bin/bash
+
+FAN="/proc/acpi/ibm/fan"
+
+if [[ -f "$FAN" ]]; then
+    echo "Fan control intercace not found: $FAN"
+    exit
+fi
+
+case "$1" in
+auto)
+    echo level auto | sudo tee $FAN
+    ;;
+full)
+    echo level 7 | sudo tee $FAN
+    ;;
+[0-7])
+    echo level $1 | sudo tee $FAN
+    ;;
+status | "")
+    cat $FAN
+    ;;
+esac
